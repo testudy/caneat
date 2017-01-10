54dbe5342248b824f1e08d2ad9d414b6b7e36ce9
Hu Jiwei <hu_jiwei@foxmail.com>
2017-01-11 01:01:52 +0800

    初步实现详情页面

--- pages/detail/index.js
+++ pages/detail/index.js
@@ -1,26 +1,35 @@
 //index.js
 //获取应用实例
-var app = getApp()
+const app = getApp();
 Page({
-  data: {
-    motto: 'Hello World',
-    userInfo: {}
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    console.log('onLoad')
-    var that = this
-    //调用应用实例的方法获取全局数据
-    app.getUserInfo(function(userInfo){
-      //更新数据
-      that.setData({
-        userInfo:userInfo
-      })
-    })
-  }
-})
+    data: {
+        post_info: {},
+    },
+
+    onLoad: function (options) {
+        console.log('onLoad');
+        console.log(options);
+
+        // TODO 删除
+        const titleId = options.title_id || 528;
+
+        wx.request({
+            url: 'https://api.1.fpm.babytree.com/api/mobile_toolcms/can_eat_detail',
+            data: {
+                'text_format': 'json',
+                'id': titleId,
+            },
+            header: {
+                'content-type': 'application/x-www-form-urlencoded',
+            },
+            success: (res) => {
+                if (res.data && res.data.data && res.data.data.post_info) {
+                    this.setData({
+                        post_info: res.data.data.post_info,
+                    });
+                    console.log(res.data.data.post_info);
+                }
+            },
+        });
+    },
+});
