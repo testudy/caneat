7985d83fc06c18f6e677593662975abdf6355d46
Hu Jiwei <hu_jiwei@foxmail.com>
2017-01-10 23:02:28 +0800

    完成搜索框

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,26 +1,92 @@
 //index.js
 //获取应用实例
-var app = getApp()
+const app = getApp();
+
 Page({
-  data: {
-    motto: 'Hello World',
-    userInfo: {}
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    console.log('onLoad')
-    var that = this
-    //调用应用实例的方法获取全局数据
-    app.getUserInfo(function(userInfo){
-      //更新数据
-      that.setData({
-        userInfo:userInfo
-      })
-    })
-  }
-})
+    data: {
+        searchShowed: false,
+        searchPlaceholder: '请输入食物名称，例如“西洋参”',
+        searchValue: "",
+    },
+
+    onLoad: function () {
+        console.log('onLoad');
+        console.log(app);
+    },
+
+    showSearch: function () {
+        this.setData({
+            searchShowed: true
+        });
+    },
+
+    hideSearch: function () {
+        this.setData({
+            searchValue: "",
+            searchShowed: false
+        });
+    },
+
+    clearSearch: function () {
+        this.setData({
+            searchValue: ""
+        });
+    },
+
+    searchTyping: function (e) {
+        this.setData({
+            searchValue: e.detail.value
+        });
+    },
+
+    search: function (e) {
+        if (!this.data.searchValue) {
+            wx.showModal({
+                title: '提示',
+                content: this.data.searchPlaceholder,
+                confirmText: '重新输入',
+                cancelText: '取消搜索',
+                success: (result) => {
+                    console.log(result);
+                    if (result.confirm) {
+                        this.showSearch();
+                    } else {
+                        this.hideSearch();
+                    }
+                },
+            });
+            return;
+        }
+
+        wx.request({
+            url: 'http://api.1.fpm.babytree.com/api/mobile_toolcms/can_eat_search',
+            data: {
+                'text_format': 'json',
+                'q': this.data.searchValue,
+            },
+            header: {
+                'content-type': 'application/x-www-form-urlencoded',
+            },
+            success: (res) => {
+                console.log(res);
+            }
+        });
+    },
+
+    onShareAppMessage: function () {
+        console.log('onShareAppMessage');
+
+        let currentPages = getCurrentPages();
+        console.log(currentPages);
+
+        let currentPage = currentPages[currentPages.length - 1];
+
+        console.log(currentPage);
+
+		return {
+      		title: '自定义分享标题',
+      		desc: '自定义分享描述',
+      		path: '/page/user?id=123'
+    	};
+    },
+});
