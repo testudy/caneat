c56613345262fefad7f08636d16bf8e6651ac53c
Hu Jiwei <hu_jiwei@foxmail.com>
2017-01-11 01:14:14 +0800

    初步实现搜索

--- pages/search/index.js
+++ pages/search/index.js
@@ -1,26 +1,38 @@
 //index.js
 //获取应用实例
-var app = getApp()
+const app = getApp();
 Page({
-  data: {
-    motto: 'Hello World',
-    userInfo: {}
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    console.log('onLoad')
-    var that = this
-    //调用应用实例的方法获取全局数据
-    app.getUserInfo(function(userInfo){
-      //更新数据
-      that.setData({
-        userInfo:userInfo
-      })
-    })
-  }
-})
+    data: {
+        post_list: [],
+        category: '',
+    },
+
+    onLoad: function (options) {
+        console.log('onLoad');
+        console.log(options);
+
+        // TODO 删除
+        const keyword = options.keyword || '米';
+
+        wx.request({
+            url: 'https://api.1.fpm.babytree.com/api/mobile_toolcms/can_eat_search',
+            data: {
+                'text_format': 'json',
+                'q': keyword,
+            },
+            header: {
+                'content-type': 'application/x-www-form-urlencoded',
+            },
+            success: (res) => {
+                console.log(res);
+                if (res.data && res.data.data && res.data.data.post_list && res.data.data.title) {
+                    this.setData({
+                        post_list: res.data.data.post_list,
+                        category: res.data.data.title,
+                    });
+                    console.log(res.data.data.post_list);
+                }
+            },
+        });
+    },
+});
